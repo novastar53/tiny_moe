8db3f4427061c75a937cc6a1f7e45933ba60fffa
Vikram Pawar <pvikram035@gmail.com>
2026-02-01 13:09:44 +0000

    Increased model weights to 1bn again

--- run_train_script.sh
+++ run_train_script.sh
@@ -1,1 +1,7 @@
+make uv
+source $HOME/.local/bin/env
+make cuda
+hf auth login --token $HF_TOKEN --no-add-to-git-credential
 nohup uv run train.py > output.log 2>&1 &
+cd training_runs/Tiny_MoE
+hf upload vikramp/Tiny_Moe_2 .
